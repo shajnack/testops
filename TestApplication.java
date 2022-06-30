af5c812d502940a81e55d5c09a16bdb9db6060b7
shajnack <108131887+shajnack@users.noreply.github.com>
2022-06-30 15:19:23 +0530

    my commit message

---
+++ TestApplication.java
@@ -0,0 +1,15 @@
+package com.fileops;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestApplication {
+
+    @Test
+    public void contextLoads() {
+    }
+}
